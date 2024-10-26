9d5209729061ecdf46e256da0dbf29f9ecee0882
Danilo Augusto <dandevweb@gmail.com>
2024-10-26 08:17:20 -0300

    feat: add user relationship in Contact model, update Contact factory, and seeders to support user contacts creation

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,22 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ContactFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'user_id'      => User::factory(),
+            'name'         => $this->faker->name(),
+            'cpf'          => $this->faker->cpf(),
+            'phone'        => $this->faker->phoneNumber(),
+            'address'      => $this->faker->streetAddress(),
+            'number'       => $this->faker->numberBetween(1, 100),
+            'complement'   => $this->faker->secondaryAddress(),
+            'neighborhood' => $this->faker->citySuffix(),
+            'city'         => $this->faker->city(),
+            'state'        => $this->faker->stateAbbr(),
+            'zip_code'     => $this->faker->postcode(),
+            'latitude'     => $this->faker->latitude(),
+            'longitude'    => $this->faker->longitude(),
         ];
     }
 }
